ed116712bcfed49e23fa38f3614782563247f3fd
Kevin Swiber <kswiber@gmail.com>
2022-03-01 21:15:39 -0800

    Preparing for v0.1.0

--- __tests__/executor.test.ts
+++
@@ -1,8 +0,0 @@
-import executor from "../src/executor";
-
-describe("executor", () => {
-  test("should run", async () => {
-    const results = await executor();
-    expect(results).toBeTruthy();
-  });
-});
